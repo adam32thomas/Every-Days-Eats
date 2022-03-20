13eb80e3979cecb11dbd6ded257266168c2561c7
Adam Belin <adambelin1992@gmail.com>
2022-03-20 14:35:24 -0400

    added listener for holiday search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
+let holidaySearch = $("#holidaySearch")
+
+function printResults() {
 fetch("https://national-api-day.herokuapp.com/api/today")
     .then(function(resp){
         console.log(resp)
 });
+};
+
+$(holidaySearch).on("click", printResults);
 
