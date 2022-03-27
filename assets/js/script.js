e20e3e666ce2f086e82110584d47f9127efe5de2
Adam Belin <adambelin1992@gmail.com>
2022-03-26 23:06:18 -0400

    added JS functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,40 @@
 let holidaySearch = $("#holidaySearch")
 let holidayResults = $("#holidayResults")
 let recipeSearch = $("#recipeSearch");
+let resultsEl = $('#results')
+
 
 function printResults() {
 fetch("https://national-api-day.herokuapp.com/api/today")
-    .then(function(response){
-        console.log(response)
-});
-};
+     .then(function(response){
+         return response.json();
+     })
+        .then(function(holidays){
+        for (let i= 0; i < holidays.length; i++) {
+            console.log(holidays[i])
+            let resultsEl = ("li");
+            let holidayEl = document.createElement("li");
+            holidayEl.textContent = response[i].html_url
+            resultsEl.appendChild(holidayEl)
+         
 
-
-
-function getHolidays() {
-    fetch("https://national-api-day.herokuapp.com/api/today")
-        .then(function (response) {
-            return response.json()
-            // resultsEl.appendChild(holidayEl);      
-        })
-        .then(function(response){
-            console.log(response)
-            let resultsEl = $("#holidayResults");
-            let holidayEl = document.createElement("p");
-            holidayEl.setAttribute("href", "https://national-api-day.herokuapp.com/api/today");
-        })
-        
-};
+ }
+ console.log(holidays)
+         })
+        }
 
 function getRecipe() {
     fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=a71abed719d541e9a4342518ad2e1fba")
         .then(function (response) {
-            // let resultsEl = $("#recipeResults")
-            // let recipeEl = document.createElement("p");
-            // console.log(response);
     })
 }
 
 $(document).ready(function () {
-    $(holidaySearch).on("click", getHolidays);
+    // $(holidaySearch).on("click", getHolidays);
     $(recipeSearch).on("click", getRecipe);
     $(holidaySearch).on("click", printResults);
 })
+
 
 
 
