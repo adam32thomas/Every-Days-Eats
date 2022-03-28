e61af7e7df82405c23a9446541c0abf2cb50cb4d
Eric Spencer <spencer22eric@gmail.com>
2022-03-28 17:52:05 -0400

    javascript additions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,6 @@ let holidayResults = $("#holidayResults")
 let recipeSearch = $("#recipeSearch");
 let resultsEl = $('#results')
 
-<<<<<<< HEAD
-
-function printResults() {
-fetch("https://national-api-day.herokuapp.com/api/today")
-     .then(function(response){
-         return response.json();
-     })
-        .then(function(holidays){
-        for (let i= 0; i < holidays.length; i++) {
-            console.log(holidays[i])
-            let resultsEl = ("li");
-            let holidayEl = document.createElement("li");
-            holidayEl.textContent = response[i].html_url
-            resultsEl.appendChild(holidayEl)
-         
-
- }
- console.log(holidays)
-         })
-        }
-=======
 // function pageDisplay() {
 //     document.querySelector('#results').innerHTML = '<li>test</li>'
 // }
@@ -47,19 +26,14 @@ function printResults() {
                 document.querySelector("#results").insertAdjacentHTML("afterbegin", html);
         })
 }
->>>>>>> AdamBelin
 
 function getRecipe() {
     fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=a71abed719d541e9a4342518ad2e1fba")
         .then(function (response) {
-<<<<<<< HEAD
-    })
-=======
             // let resultsEl = $("#recipeResults")
             // let recipeEl = document.createElement("p");
             // console.log(response);
         })
->>>>>>> AdamBelin
 }
 
 $(document).ready(function () {
@@ -68,10 +42,4 @@ $(document).ready(function () {
     $(holidaySearch).on("click", printResults);
 })
 
-<<<<<<< HEAD
-
-
-
-=======
 // pageDisplay();
->>>>>>> AdamBelin
