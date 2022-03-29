18c96581503b0fecdbf1dbae580c46745d23b0ca
olacase@gmail.com <olacase@gmail.com>
2022-03-29 18:01:06 -0500

    get recipe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 let holidaySearch = $("#holidaySearch");
 let holidayResults = $("#holidayResults");
 let recipeSearch = $("#recipeSearch");
-let resultsEl = $('#results');
-
+let resultsEl = $('#results'); 
+const recipeResult = $('#recipeResult');
+let recipeButton = $('#recipeIndex');
 
 function printResults() {
     fetch("https://national-api-day.herokuapp.com/api/today")
@@ -21,14 +22,35 @@ function printResults() {
         })
 }
 
-// function getRecipe() {
-//     fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=a71abed719d541e9a4342518ad2e1fba")
-//         .then(function (response) {
-            
-//         })
-// }
+
+const loadRecipe= async () => {
+    try {
+        const res = await fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=a71abed719d541e9a4342518ad2e1fba');
+        const recipeList = await res.json();
+        // displayRecipe(recipeList);
+        console.log(recipeList)
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayRecipe = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <ul id="recipe">
+               
+            </ul>
+        `;
+        })
+        console.log(recipeList)
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+
 
 $(document).ready(function () {      
-    // $(recipeSearch).on("click", getRecipe);
+    $(recipeButton).on("click", loadRecipe);
     $(holidaySearch).on("click", printResults);
 });
